d0496821c15a8d8f4676d28c59dde916bfda40c0
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2017-02-15 22:45:44 -0500

    add reimbursement to db poc

--- class/reimburse/models.py
+++ class/reimburse/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Reimbursement(models.Model):
+    description = models.CharField(max_length=200)
+    request_date = models.DateTimeField('date requested')
+
+    def __str__(self):
+        return self.description
