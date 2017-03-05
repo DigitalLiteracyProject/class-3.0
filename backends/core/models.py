184c2ee8fc37f16db1cff3717818bde7ea35bda3
icasdri <icasdri@gmail.com>
2017-03-05 14:39:45 -0500

    Start on some models for core

--- backends/core/models.py
+++ backends/core/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Classroom(models.Model):
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self):
+        return "Classroom[{}]: (t: {}) {}".format(self.id, self.teacher.id, self.description)
+
+
+class Student(models.Model):
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "Student[{}]: (c: {}) {}".format(self.id, self.classroom.id, self.name)
